1735bd2b6ffda724cc72b00050f375544396ab2b
Lucas Allan Amorim <lucas.allan@gmail.com>
2012-06-14 12:56:04 -0300

    added support to post requests

--- lib/aweber/base.rb
+++ lib/aweber/base.rb
@@ -8,23 +8,27 @@ module AWeber
     def account
       accounts.first.last
     end
-    
+
     def get(uri)
       response = oauth.get(expand(uri))
       handle_errors(response, uri)
       parse(response) if response
     end
-    
+
+    def post(uri, body={})
+      oauth.post(uri, body.to_json, {"Content-Type" => "application/json"})
+    end
+
     def delete(uri)
       oauth.delete(expand(uri))
     end
-    
+
     def put(uri, body={})
       oauth.put(uri, body.to_json, {"Content-Type" => "application/json"})
     end
 
   private
-    
+
     def handle_errors(response, uri)
       if response.is_a? Net::HTTPNotFound
         raise NotFoundError, "Invalid resource uri.", caller
